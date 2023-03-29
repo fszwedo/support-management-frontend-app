ce8cc736b275c9a5cd8a29a47b61c6781f06e536
FIlip <filip.szwedo@gmail.com>
2023-03-29 13:21:22 +0200

    backend url fixed

--- src/constants.tsx
+++ src/constants.tsx
@@ -1,5 +1,5 @@
-//export const BACKEND_URL = 'https://zendesk-backend.onrender.com'
-export const BACKEND_URL = 'http://localhost:3001'
+export const BACKEND_URL = 'https://zendesk-backend.onrender.com'
+//export const BACKEND_URL = 'http://localhost:3001'
 // use 'http://localhost:3001' if you run backend locally
 
 export const APPPATH = ''
